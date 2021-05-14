5735cee0d1e23f32f674765e63b670c8866507cf
raddymaster <antellitocci@gmail.com>
2021-05-13 21:05:46 -0400

    added color coding based on if current time is before, same, or after the time slot

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,12 +1,13 @@
 //get reference to current day section & set current day
 $("#currentDay").text(moment().format("dddd, MMMM D, YYYY"));
 //set time block range
-var startTime = 9;
-var endTime = 17;
+var startTime = 9; //9 am
+var endTime = 17; //5 pm
 
 //create time block elements
 for (var i = startTime; i < endTime + 1; i++)
 {
+    var currentTime = moment().format("hA");
     //create time
     var timeText = moment().hour(i).format("hA");
     //create time block div row
@@ -15,6 +16,24 @@ for (var i = startTime; i < endTime + 1; i++)
     var timeHourElem = $("<div>").addClass("hour col-2 col-sm-1").text(timeText).attr("style", "padding-top: 30px;");
     //create task text area element
     var textAreaElem = $("<textarea>").addClass("description col-8 col-sm-10");
+
+    //create variables to compare current time and timeText in appropriate ISO format
+    var currTimeFormat = moment(currentTime, "hA").format();
+    var timeTextFormat = moment(timeText, "hA").format();
+
+    //color code the text area based on past / present / future
+    if (moment(timeTextFormat).isBefore(currTimeFormat))
+    {
+        textAreaElem.addClass("past");
+    }
+    else if (moment(timeTextFormat).isSame(currTimeFormat))
+    {
+        textAreaElem.addClass("present");
+    }
+    else 
+    {
+        textAreaElem.addClass("future");
+    }
     //create save button element and add save icon to it
     var buttonElem = $("<button>").addClass("btn saveBtn col-2 col-sm-1").html("<i class='fas fa-save'></i>");
 
@@ -24,4 +43,11 @@ for (var i = startTime; i < endTime + 1; i++)
     timeRowElem.append(buttonElem);
     //append items to page
     $("#timeBlocks").append(timeRowElem);
+
+    //add id to each div in here somewhere
 }
+
+//add tasks to page from local storage
+//click into time block enter event
+//click save button and it is saved to local storage
+
