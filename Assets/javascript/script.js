c694a92abd79fbc407b8b7e216913d6d2f122963
raddymaster <antellitocci@gmail.com>
2021-05-15 10:54:21 -0400

    added audit function to dynamically update color of time blocks every 15 min and adjusted css styling to match personal taste

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -4,11 +4,10 @@ $("#currentDay").text(moment().format("dddd, MMMM D, YYYY"));
 var startTime = 9; //9 am
 var endTime = 17; //5 pm
 
-//create time block elements
+//create time block elements and add to html
 for (var i = startTime; i < endTime + 1; i++)
 {
-    var currentTime = moment().format("hA");
-    //create time
+    //create time text
     var timeText = moment().hour(i).format("hA");
     //create time block div row
     var timeRowElem = $("<div>").addClass("row row-cols-3 time-block px-2");
@@ -16,24 +15,10 @@ for (var i = startTime; i < endTime + 1; i++)
     var timeHourElem = $("<div>").addClass("hour col-2 col-sm-1").text(timeText).attr("style", "padding-top: 30px;");
     //create task text area element
     var textAreaElem = $("<textarea>").addClass("description col-8 col-sm-10");
-
-    //create variables to compare current time and timeText in appropriate ISO format
-    var currTimeFormat = moment(currentTime, "hA").format();
-    var timeTextFormat = moment(timeText, "hA").format();
-
-    //color code the text area based on past / present / future
-    if (moment(timeTextFormat).isBefore(currTimeFormat))
-    {
-        textAreaElem.addClass("past");
-    }
-    else if (moment(timeTextFormat).isSame(currTimeFormat))
-    {
-        textAreaElem.addClass("present");
-    }
-    else 
-    {
-        textAreaElem.addClass("future");
-    }
+    
+    //pass textArea and time text to audit time to style appropriately
+    auditTime(textAreaElem, timeText);
+    
     //create save button element and add save icon to it
     var buttonElem = $("<button>").addClass("btn saveBtn col-2 col-sm-1").html("<i class='fas fa-save'></i>");
 
@@ -47,6 +32,40 @@ for (var i = startTime; i < endTime + 1; i++)
     //add id to each div in here somewhere
 }
 
+//audit task to assign appropriate color
+function auditTime(textAreaElem, timeText){
+    //create variable to compare current time and timeText in appropriate ISO format
+    var timeTextFormat = moment(timeText, "hA").format();
+    //color code the text area based on past / present / future
+    if (moment().isAfter(timeTextFormat, "hour")){
+        textAreaElem.addClass("past");
+        textAreaElem.removeClass("present");
+        textAreaElem.removeClass("future");
+    }
+    else if (moment().isSame(timeTextFormat, "hour")){
+        textAreaElem.addClass("present");
+        textAreaElem.removeClass("past");
+        textAreaElem.removeClass("future");
+    }
+    else {
+        textAreaElem.addClass("future");
+        textAreaElem.removeClass("past");
+        textAreaElem.removeClass("presenet");
+    }
+};    
+
+
+function saveTasks()
+{
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+//audit time block time colors every 15 minutes
+setInterval(function(){
+    $(".description").each(function(index, elem){
+        auditTime(elem);
+    });
+}, (1000*60)*15);
 //add tasks to page from local storage
 //click into time block enter event
 //click save button and it is saved to local storage
